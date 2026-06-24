ba05325b4c35689c2815e084be8b5bff158b69a2
SanGeJi <1940384762@qq.com>
2026-06-24 23:25:01 +0800

    Add shop/src/main/java/com/shop/entity/SpecOption.java

---
+++ shop/src/main/java/com/shop/entity/SpecOption.java
@@ -0,0 +1,10 @@
+package com.shop.entity;
+
+public class SpecOption {
+    private Long id;
+    private Long specId;
+    private String value;
+    public Long getId() { return id; } public void setId(Long v) { this.id = v; }
+    public Long getSpecId() { return specId; } public void setSpecId(Long v) { this.specId = v; }
+    public String getValue() { return value; } public void setValue(String v) { this.value = v; }
+}
