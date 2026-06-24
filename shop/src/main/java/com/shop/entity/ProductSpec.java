6fecdec9cf70ce07f38a5ebf2db621884b25bbd8
SanGeJi <1940384762@qq.com>
2026-06-24 23:24:52 +0800

    Add shop/src/main/java/com/shop/entity/ProductSpec.java

---
+++ shop/src/main/java/com/shop/entity/ProductSpec.java
@@ -0,0 +1,10 @@
+package com.shop.entity;
+
+public class ProductSpec {
+    private Long id;
+    private Long productId;
+    private String name;
+    public Long getId() { return id; } public void setId(Long v) { this.id = v; }
+    public Long getProductId() { return productId; } public void setProductId(Long v) { this.productId = v; }
+    public String getName() { return name; } public void setName(String v) { this.name = v; }
+}
